31e7b6fbea5a025dacea9ea61c442c6c1988df0b
jameshcoppens <james.coppens@compass.com>
2016-04-13 09:57:06 -0400

    Update to the 2nd exercise to finish calculation within ruby

--- 2/ruby_2.rb
+++ 2/ruby_2.rb
@@ -1,17 +1,23 @@
 include Math 
 
 $sequence = Array.new
+$sequence.push(1)
 
 def generate_fib_seq
 
-  while sequence[sequence.size-1] < 4000000 do
-    sequence[sequence.size] = sequence[sequence.size-1] + sequence[sequence.size-2]  
+  while $sequence[$sequence.size-1] < 4000000 do
+    $sequence[$sequence.size] = $sequence[$sequence.size-1] + $sequence[$sequence.size-2]  
   end
   
 end
 
 def sum_fib_seq_even
+
+  sum = 0
+  $sequence.each{ |num,index| if num % 2 == 0 then sum += num end}
+  puts sum
+
 end
 
 generate_fib_seq
-
+sum_fib_seq_even
